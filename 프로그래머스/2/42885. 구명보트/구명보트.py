84573555159974f54cd46963e4c5318b01ae7bf1
Wonna_won <90510391+hwon820@users.noreply.github.com>
2024-04-04 17:35:05 +0900

    [level 2] Title: 구명보트, Time: 9.60 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,20 @@
+def solution(people, limit):
+    answer = 0
+    
+    people.sort()
+
+    l = 0; r = len(people) - 1
+
+    while not l > r:
+        if l == r:
+            answer += 1
+            break
+
+        if people[l] + people[r] <= limit:
+            answer += 1
+            l += 1; r -= 1
+        else:
+            answer += 1
+            r -= 1
+
+    return answer
