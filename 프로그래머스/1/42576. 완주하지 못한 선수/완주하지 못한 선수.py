79009e6d88c2ed7c92895871730790208964e03e
Wonna_won <90510391+hwon820@users.noreply.github.com>
2025-09-29 21:55:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 71.58 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,27 +1,10 @@
-def solution(participant, completion):
-    answer = ''
-    p = {}
-    c = {}
-    
-    for i in participant:
-        if i in p:
-            p[i] += 1
-        else:
-            p[i] = 1
-    
-    for i in completion:
-        if i in c:
-            c[i] += 1
-        else:
-            c[i] = 1
-    
-    if set(participant) == set(completion):
-        for i in p:
-            if p[i] - c[i] == 1:
-                return i
-    else:
-        answer += str(set(participant) - set(completion))[2:-2]
-    
-    return answer
+from collections import Counter
 
-## str.rstrip() : 문자열에서 필요없는 부분 지우기
+def solution(participant, completion):
+    
+    part = Counter(participant)
+    comp = Counter(completion)
+    
+    answer = part - comp
+    
+    return answer.most_common()[0][0]
