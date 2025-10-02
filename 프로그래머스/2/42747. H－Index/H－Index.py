36a782fdd9e0935c836f93f25e2238704ec40f3e
Wonna_won <90510391+hwon820@users.noreply.github.com>
2025-10-02 23:31:04 +0900

    [level 2] Title: H-Index, Time: 0.16 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,17 +1,13 @@
-# [0, 3, 3, 5, 6]
-# 0 -> 0번 이상 인용 (5-0)편 / 0번 이하 인용 1편
-# 1 -> 3번 이상 인용 (5-1)편 / 3번 이하 인용 2편  (지금까진)
-# 2 -> 3번 이상 인용 (5-2)편 / 3번 이하 인용 3편   h = h
-
-# 3 -> 5번 이상 인용 (5-3)편 / 5번 이하 인용 4편
-# 4 -> 6번 이상 인용 (5-4)편 / 6번 이하 인용 5편
+#  0  1  2  3  4
+#  1  2  3  4  5
+# [0, 1, 3, 5, 6]
+# h = 6, 개수: idx + 1 개
 
 
 def solution(citations):
-    citations.sort(reverse = True)
-    h = 0
-    for idx, c in enumerate(citations, start = 1):
-        if c >= idx:
-            h = idx
-    
-    return h
+    citations = sorted(citations, reverse = True)
+    for i in range(len(citations)):
+        if citations[i] < i+1:
+            return i
+    return len(citations)
+            
