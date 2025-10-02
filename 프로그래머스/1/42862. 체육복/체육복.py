fd5c1b73b42a7c65bd86d4ce955df2c98ac2f65c
Wonna_won <90510391+hwon820@users.noreply.github.com>
2025-10-02 22:43:08 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,24 +1,13 @@
 def solution(n, lost, reserve):
-    answer = 0
-
-    lost.sort()
-    reserve.sort()
-
-    # 여벌이 있는 학생
-    new_lost = []
-    for i in lost:
-        if i in reserve:
-            reserve.remove(i)
+    reserve_ = list(set(reserve) - set(lost))
+    lost_ = list(set(lost) - set(reserve))
+    lst = 0
+    for i in lost_:
+        if i - 1 in reserve_:
+            reserve_.remove(i-1)
+        elif i + 1 in reserve_:
+            reserve_.remove(i+1)
         else:
-            new_lost.append(i)
-    lost = new_lost
-
-    # 여벌이 없는 학생
-    for i in range(len(lost)):
-        for j in range(len(reserve)):
-            if lost[i] - reserve[j] == 1 or lost[i] - reserve[j] == -1:
-                answer += 1
-                reserve[j] = -1    # 빌려줬으면 표시
-                break
-
-    return n - (len(lost) - answer)
+            lst += 1
+    
+    return n - lst
