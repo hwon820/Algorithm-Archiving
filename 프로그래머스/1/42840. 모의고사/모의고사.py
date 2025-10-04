bbc829225b42913902d9eb29f9b8432d13c99456
Wonna_won <90510391+hwon820@users.noreply.github.com>
2025-10-04 21:01:11 +0900

    [level 1] Title: 모의고사, Time: 2.43 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,23 @@
-## 1번 : 1, 2, 3, 4, 5                  (5)
-## 2번 : 2, 1, 2, 3, 2, 4, 2, 5         (8)
-## 3번 : 3, 3, 1, 1, 2, 2, 4, 4, 5, 5   (10)
-
-# 문제 개수가 103개
-# 문제 인덱스의 % size로 인덱싱
+# 1 2 3 4 5
+# 2 1 2 3 2 4 2 5
+# 3 3 1 1 2 2 4 4 5 5
 
 def solution(answers):
-    
-    id_1 = [i for i in range(1, 6)]; id_2 = [2, 1, 2, 3, 2, 4, 2, 5]; id_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    status = [0, 0, 0]
+    student_1 = [1, 2, 3, 4, 5]; n1 = len(student_1)
+    student_2 = [2, 1, 2, 3, 2, 4, 2, 5]; n2 = len(student_2)
+    student_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]; n3 = len(student_3)
+    cnt = [0, 0, 0]
     for i in range(len(answers)):
-        if answers[i] == id_1[i % 5]:  status[0] += 1  # 1번
-        if answers[i] == id_2[i % 8]:  status[1] += 1  # 2번
-        if answers[i] == id_3[i % 10]: status[2] += 1  # 3번
+        if student_1[i % n1] == answers[i]:
+            cnt[0] += 1
+        if student_2[i % n2] == answers[i]:
+            cnt[1] += 1
+        if student_3[i % n3] == answers[i]:
+            cnt[2] += 1
         
-    max_score = max(status); ans = []
-    for i in range(3):
-        if status[i] == max_score:
-            ans.append(i + 1)
-        
+    max_ = max(cnt); ans = []
+    for j in range(len(cnt)):
+        if cnt[j] == max_:
+            ans.append(j+1)
+            
     return ans 
