929de9a14dba3267dbe715f3831172341b2868fd
Wonna_won <90510391+hwon820@users.noreply.github.com>
2025-10-07 15:34:00 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,7 @@
-# 경우의 수 정의
-# 한 카테고리에 r 개: r + 1 개의 경우의 수(안 입는 거 포함) 
-# 전체 카테고리 (r+1)을 곱하고 -1 (nothing)
-
+# Counter 사용
+from collections import Counter
+from functools import reduce
 def solution(clothes):
-    maps = {}
-    for c in clothes:
-        if c[1] in maps.keys():
-            maps[c[1]] += 1
-        else:
-            maps[c[1]] = 1
-    ans = 1
-    for i in maps.values(): ans *= i + 1
-    return ans - 1
+    c_map = Counter([typ for cloth, typ in clothes])
+    ans = reduce(lambda x, y: x*(y+1), c_map.values(), 1) - 1
+    return ans
