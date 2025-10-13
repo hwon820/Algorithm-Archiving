400efb9c7007d36477652340eb37a4818a1c17dd
Wonna_won <90510391+hwon820@users.noreply.github.com>
2025-10-13 12:06:36 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,13 +1,12 @@
-# 가로 길이*2 + 세로 길이*2 + 4
+# 전체 개수 = (yellow 가로 + 2) * (yelllow 세로 + 2) = 12
+# yellow 개수 24 -> 24 / 1, 12 / 2, 6 / 4
+#                   26 * 3, 14 * 4, 8 * 6
 
 def solution(brown, yellow):
     
-    ans = []
+    total = brown + yellow
     
     for i in range(1, yellow + 1):
-        if yellow % i == 0:
-            y_x = i; y_y = yellow // i
-            if y_x*2 + y_y*2 + 4 == brown:
-                ans.append(y_x + 2); ans.append(y_y + 2)
-                ans.sort(reverse = True)
-                return ans
+        if yellow % i == 0 and (i + 2) * (yellow//i + 2) == total:
+            row = min(i, yellow//i) + 2; col = max(i, yellow//i) + 2
+            return [col, row]
