ddf6f58b6b6b57ac2ed3407eb63c2feaa062efe9
Wonna_won <90510391+hwon820@users.noreply.github.com>
2025-10-14 12:41:19 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,7 +1,12 @@
-# Counter 사용
-from collections import Counter
+# 최종 경우의 수: (각 의상 가짓수 + 1) 누적 곱 - 1 (안 입는 경우)
 from functools import reduce
 def solution(clothes):
-    c_map = Counter([typ for cloth, typ in clothes])
-    ans = reduce(lambda x, y: x*(y+1), c_map.values(), 1) - 1
-    return ans
+    dic = {}; ans = 1
+    
+    for clo, typ in clothes:
+        if typ in dic:
+            dic[typ] += 1
+        else:
+            dic[typ] = 2
+    ans = reduce(lambda x, y: x * y, dic.values(), 1)
+    return ans - 1
